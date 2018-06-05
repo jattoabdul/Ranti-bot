6673778ed610c2b331e2da7d8348a798b122cb35
jattoabdul <jattoade@gmail.com>
2018-06-05 13:04:08 +0100

    modify the show task action to send task for today or tomorrow queries

--- app/actions.py
+++ app/actions.py
@@ -1,6 +1,6 @@
 from app.utils.gappshelper import GappsHelper
 import time
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from config import get_env
 
 
@@ -21,33 +21,66 @@ class Actions:
 				'*Hey {},* Today is the check-in day for your writeup titled\n'
 				'`{}`.\n\n'
 				'Whats the status of the article?\n'
-				'PS: Please reply to this thread, the managers will review and reply you ASAP').format(str(index + 1), row['Next Check-In'], row['Name'], row['Most Recent Learning Experience you\'d like to write about'])
+				'PS: Please reply to this thread, the managers will review and reply you ASAP').format(str(index + 1),
+																									   row[
+																										   'Next Check-In'],
+																									   row['Name'], row[
+																										   'Most Recent Learning Experience you\'d like to write about'])
 			self.slackhelper.post_message(text_detail, recipient)
 		return None
 
-	def show_tasks(self, date=None):
-		if date in ['today', 'tomorrow']:
-			return {'text': 'sending task for {}'.format(date)}
-		# elif re.match('', date) == False:
-		# 	response_body = {'text': 'Invalid Date/Day Param - `/ranti help` for available commands'}
+	def _convert_to_date(self, date_string):
+		today = date.today()
+		if date_string == 'today':
+			return today
+		elif date_string == 'yesterday':
+			return today - timedelta(days=1)
+		elif date_string == 'tomorrow':
+			return today + timedelta(days=1)
 		else:
-			recipient = self.user_info['user']['id']
+			return today
+
+	def show_tasks(self, date=None):
+		if date in ['today', 'tomorrow', 'yesterday']:
+			day_date_param = self._convert_to_date(date)
+			task_cells = list(filter(
+				lambda x: datetime.strptime(self._num_suffix(x['Next Check-In']), '%d %B %Y').date() == day_date_param,
+				self.sheet))
+			if task_cells:
+				self._perform_send_action(task_cells)
+			else:
+				return {'text': 'No task assigned to be checked in {}, try another date'.format(date)}
+		# below elif statement to be used to check if passed in param matches the desired format {dth-month-yyyy}
+		# elif re.match('desired_format{dth-month-yyyy}', date):
+		else:
 			date_param = date.replace('-', ' ')
 			task_cells = list(filter(lambda x: x['Next Check-In'] == date_param, self.sheet))
 			if task_cells:
-				for index, row in enumerate(task_cells):
-					text_detail = (
-						'*Task #{} for {}:* \n\n'
-						'*Hey {},* Today is the check-in day for your writeup titled\n'
-						'`{}`.\n\n'
-						'Whats the status of the article?\n'
-						'PS: Please reply to this thread, the managers will review and reply you ASAP').format(str(index + 1), row['Next Check-In'], row['Name'], row['Most Recent Learning Experience you\'d like to write about'])
-					self.slackhelper.post_message(text_detail, recipient)
-				return None
+				self._perform_send_action(task_cells)
 			else:
 				return {'text': 'No task assigned to be checked in on this date, try another date'}
+		# else:
+		#   if the date format doesn't match the desired format or in ['today','tomorrow','yesterday'],return error msg
+		# 	response_body = {'text': 'Invalid Date/Day Param - `/ranti help` for available commands'}
+
+	def _perform_send_action(self, task_cells):
+		recipient = self.user_info['user']['id']
+		for index, row in enumerate(task_cells):
+			text_detail = (
+				'*Task #{} for {}:* \n\n'
+				'*Hey {},* Today is the check-in day for your writeup titled\n'
+				'`{}`.\n\n'
+				'Whats the status of the article?\n'
+				'PS: Please reply to this thread, the managers will review and reply you ASAP').format(
+				str(index + 1), row['Next Check-In'], row['Name'],
+				row['Most Recent Learning Experience you\'d like to write about'])
+			self.slackhelper.post_message(text_detail, recipient)
+		return None
 
 	def help(self):
+		"""
+		Return the Available commands in the system and their usage format
+		"""
 		return {
 			'text': 'Available Commands: \n `/ranti my-task e.g. /ranti my-task` \n To get task assigned to you.\n'
 					' \n `/ranti show-task [date]{dth-month-year} e.g. /ranti show-task 5th-june-2018` \n Show all tasks for a particular date \n'
